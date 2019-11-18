32eda796cc8a6f540734c7e5d9c4a43900b0aa8e
Tony Rispoli <trispoli992@gmail.com>
2019-11-17 19:17:58 -0600

    Updated Member class with a toString to save the members into a tab-delimited file as it came in

--- Member.java
+++ Member.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
 import java.io.Serializable;
+/**
+ * @author tony rispoli
+ * this class creates Member objects to be able to calculate the risk associated with their health
+ * the Member object will be used in just about every other class, as it is a cornerstone
+ */
 public class Member implements Serializable {
 	private String firstName, lastName, cancer, diabetes, alz;
     private int age, height, weight, bps, bpd;
+    //getters and setters
     public String getFirstName() {
         return firstName;
     }
@@ -63,7 +69,7 @@ public class Member implements Serializable {
     public void setAlz(String alz) {
     	this.alz = alz;
     }
-    
+    //creates member object with all necessary input
     public Member(String firstName, String lastName, int age, int height, int weight, int bps,
     		int bpd, String cancer, String diabetes, String alz) {
         setFirstName(firstName);
@@ -76,6 +82,7 @@ public class Member implements Serializable {
         setCancer(cancer);
         setDiabetes(diabetes);
         setAlz(alz);
+        getMembersAsString(this);
     }
 
     public Member() { // used for xml serialization
@@ -90,18 +97,36 @@ public class Member implements Serializable {
     	diabetes = "";
     	alz = "";
     }
-    public static void getMembersAsString(Member member) {
-        // gets a list of numbers & prints it nicely to the screen
-    	System.out.println(member.getLastName() + ", " + member.getFirstName());
-    	System.out.println("Age:          " + member.getAge());
-    	System.out.println("Height:       " + member.getHeight() + " in");
-    	System.out.println("Weight:       " + member.getWeight() + " lbs");
-    	System.out.println("BP Syst:      " + member.getBPS());
-    	System.out.println("BP Dias:      " + member.getBPD());
-    	System.out.println("Cancer:       " + member.getCancer());
-    	System.out.println("Diabetes:     " + member.getDiabetes());
-    	System.out.println("Alzheimer's:  " + member.getAlz());
-    	System.out.println("----------------------");
+    /*
+     * @name getMembersAsString
+     * this will print a member's information in a pretty format to the screen
+     * @param Member member uses object to grab information already set
+     * @return void but prints to screen 
+     */
+    public static void getMembersAsString(Member member) {       
+     	System.out.println(member.getLastName() + ", " + member.getFirstName());
+     	System.out.println("Age:           " + member.getAge());
+     	System.out.println("Height:        " + member.getHeight() + " in");
+     	System.out.println("Weight:       " + member.getWeight() + " lbs");
+     	System.out.println("BP Syst:      " + member.getBPS());
+     	System.out.println("BP Dias:       " + member.getBPD());
+     	System.out.println("Cancer:         " + member.getCancer());
+     	System.out.println("Diabetes:       " + member.getDiabetes());
+     	System.out.println("Alzheimer's:    " + member.getAlz());
+     	System.out.println("----------------------");
+     }
+    /*
+     * @name toString
+     * used for saving to the text file, as it keeps the original format it read it from
+     * @param Member m works backwards and stores member information in a tab-delimited list
+     * @return mem, the tab-delimited list
+     */
+    public static String toString(Member m) {
+    	String mem = (m.getFirstName() + "\t" + m.getLastName() + "\t" + m.getAge() + "\t" + m.getHeight() + "\t" 
+        + m.getWeight() + "\t" + m.getBPS() + "\t" + m.getBPD() + "\t" + m.getCancer() + "\t" 
+    	+ m.getDiabetes() + "\t" + m.getAlz());
+    	return mem;
     }
+
 
 }
