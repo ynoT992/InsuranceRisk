b221d7d0afb8cf8f469234555931541b90109c67
Tony Rispoli <trispoli992@gmail.com>
2019-11-17 19:15:40 -0600

    Updated InsuranceScore.java to add a toString function that will help with the JSON formatting

--- InsuranceScore.java
+++ InsuranceScore.java
@@ -1,9 +1,12 @@
 
 public class InsuranceScore {
+// @author tony rispoli
 // this class will contain InsuranceScore objects that will store the member's name, score, and risk
 	private static String name, risk;
 	private static int score;
-	
+	/*
+	 * getters and setters to get required inputs for an InsuranceScore object
+	 */
 	public String getName() {
 		return name;
 	}
@@ -22,11 +25,15 @@ public class InsuranceScore {
 	public void setRisk(String risk) {
 		this.risk = risk;
 	}
-	
+	/*
+	 * InsuranceScore object contains nothing more than member's first and last name, their risk score,
+	 *  and the verdict associated with score
+	 */
 	InsuranceScore(String name, int score, String risk) {
 		setName(name);
 		setScore(score);
 		setRisk(risk);
+		toString(this);
 	}
 	
 	InsuranceScore() {
@@ -34,11 +41,31 @@ public class InsuranceScore {
 		score = 0;
 		risk = "";
 	}
+	/*
+	 * @name getScoreAsString
+	 * to be used for the writeAssessmentToScreen function to format assessment
+	 * @param InsuranceScore InsScore an object that we need information from
+	 * @return void, but prints to the screen through system.out
+	 */
 	public static void getScoreAsString(InsuranceScore InsScore) {       
-	 	   // gets a list of numbers & prints it nicely to the screen
+	 	   // prints assessment nicely to the screen
 	     	System.out.println("Name:           " + InsScore.getName());
-	     	System.out.println("Score:        " + InsScore.getScore());
-	     	System.out.println("Verdict:       " + InsScore.getRisk());
-	     	System.out.println("----------------------");
-	     }
+	     	System.out.println("Score:          " + InsScore.getScore());
+	     	System.out.println("Verdict:        " + InsScore.getRisk());
+	     	System.out.println("---------------------------------");
+	}
+	/*
+	 * @name toString
+	 * this formats each InsuranceScore object into a JSON format
+	 * @param InsuranceScore ins will need the info from the object
+	 * @return score - a string that has JSON formatting
+	 */
+	public static String toString(InsuranceScore ins) {
+		String score = ("\t { \n" +
+		"\t\t Name: " + ins.getName() + "\n" +
+		"\t\t Score: " + ins.getScore() + "\n" +
+		"\t\t Verdict: " + ins.getRisk() + "\n" +
+		"\t } \n");
+		return score;
+	}
 }
